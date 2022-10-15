ad8c63e72ccd35fa525339d066fd2a58b5e9e8e2
andre2ar <andre2ar@outlook.com>
2022-10-15 19:56:05 +0100

    uncaughtException and unhandledRejection

--- server/index.js
+++ server/index.js
@@ -4,3 +4,6 @@ import config from "./config.js";
 
 server.listen(config.port)
 .on('listening', () => logger.info(`Server running ${config.port}`))
+
+process.on('uncaughtException', (error) => logger.error(`UnhandledException happend: ${error.stack || error}`))
+process.on('unhandledRejection', (error) => logger.error(`UnhandledRejection happend: ${error.stack || error}`))
